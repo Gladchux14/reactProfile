85d415bdec1695a3003acc8e7b84d51a7ec5e2f8
Glad Chukwu <nellg.gc@gmail.com>
2024-05-26 13:49:17 -0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  "./src/Components/**/*.{js,ts,jsx,tsx}",
   content: ["./src/**/*.{html,js,jsx,ts,tx}"],
   theme: {
     extend: {},
